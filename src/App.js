9e08fd32ad9b7d38c618b4b157821f6003117bd1
RafaelBD <rafa.bediaz@gmail.com>
2024-06-09 15:55:13 -0600

    redirect

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import './App.css';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from './Components/Login/Login';
 import Topbar from './Components/Topbar/Topbar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 function App() {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, isLoading, error } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Oops... {error.message}</div>;
+
+  console.log("Is Authenticated:", isAuthenticated);
+
   return (
     <Router>
-          <div className="App">
-        {isAuthenticated ? <>
-          <Topbar />
-        </>
-      : <LoginButton />}
-    </div>
+      <div className="App">
+        {isAuthenticated ? (
+          <>
+            <Topbar />
+          </>
+        ) : (
+          <LoginButton />
+        )}
+      </div>
     </Router>
-
   );
 }
 
