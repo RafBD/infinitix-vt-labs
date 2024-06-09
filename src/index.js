28adaebc91fc1c940b690c8f91869e12c2b95802
RafaelBD <rafa.bediaz@gmail.com>
2024-06-08 21:34:16 -0600

    claves cambiadas

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
+import config from './auth_config.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Auth0Provider
-    domain = "dev-obinb706lll8exre.us.auth0.com"
-    clientId = "sFYv4ZNECXObneWy4Tiy1pBF5MXGUK48"
+    domain = {config.domain}
+    clientId = {config.clientId}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}>
