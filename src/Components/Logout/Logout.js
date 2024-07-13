f1e28da5083b137b812c1f5a183b9db916c1b1a4
RafaelBD <rafa.bediaz@gmail.com>
2024-07-13 17:02:21 -0600

    Update structure draw

--- src/Components/Logout/Logout.js
+++ src/Components/Logout/Logout.js
@@ -4,11 +4,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 export const LogOutButton = () => {
     const { logout } = useAuth0();
     return(
-        <button className='flex justify-center items-center gap-2' onClick = {() => logout()}>
-        <i className="pi pi-sign-out"></i>
-        Cerrar Sesión
-        </button>
-        /*
         <button
         onClick={() =>
             logout({
@@ -17,6 +12,11 @@ export const LogOutButton = () => {
         }
         >
             Cerrar Sesión
+        </button>
+        /*
+        <button className='flex justify-center items-center gap-2' onClick = {() => logout()}>
+        <i className="pi pi-sign-out"></i>
+        Cerrar Sesión
         </button>
         */
     )
