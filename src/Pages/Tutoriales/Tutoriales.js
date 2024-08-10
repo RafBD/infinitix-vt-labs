e4dd66a5d22f84ca29946b353074bf3bcfffe33d
RafaelBD <rafa.bediaz@gmail.com>
2024-08-10 01:03:44 -0600

    Envolver en div para mejorar la vista en móvil

--- src/Pages/Tutoriales/Tutoriales.js
+++ src/Pages/Tutoriales/Tutoriales.js
@@ -8,17 +8,32 @@ export default function Tutoriales() {
         <hr className="mb-3 mx-3 border-top-2 border-bottom-1" />
 
         <div className='video-grid'>
-            <div className='video-wrapper'>
-                <h2 className='titulo-video text-center p-3'>Login</h2>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/DSapUITOXm4?si=eHaseKDHqgiMRcKm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div>
+                <div className='mb-3'>
+                    <h2 className='titulo-video text-center p-2'>Login</h2>
+                </div>
+                <div className='video-wrapper'>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/DSapUITOXm4?si=eHaseKDHqgiMRcKm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
             </div>
-            <div className='video-wrapper'>
-                <h2 className='titulo-video text-center p-3'>Menú | Configuración de Usuario</h2>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/tX1uhw5QEXY?si=t2lKyaNijFFBqMVr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+            <div>
+                <div className='mb-3'>
+                    <h2 className='titulo-video text-center p-2'>Menú | Configuración de Usuario</h2>
+                </div>
+                <div className='video-wrapper'>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/tX1uhw5QEXY?si=t2lKyaNijFFBqMVr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
             </div>
-            <div className='video-wrapper'>
-                <h2 className='titulo-video text-center p-3'>SideBar (Barra Lateral)</h2>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/agxSdRJLZhA?si=vKH3lrrfojCFgPka" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+
+            <div>
+                <div className='mb-3'>
+                    <h2 className='titulo-video text-center p-2'>SideBar (Barra Lateral)</h2>
+                </div>
+                <div className='video-wrapper'>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/agxSdRJLZhA?si=vKH3lrrfojCFgPka" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
             </div>
         </div>
     </div>
